55e20a750d4c2f3cdd94e453b46fa74a5aa7e6aa
me <caizh@umich.edu>
2021-11-29 20:55:53 -0500

    can't run constructor

--- hashmap1.cpp
+++ hashmap1.cpp
@@ -6,7 +6,7 @@ using namespace std;
 class SerializableHashMap
 {
 private: 
-    char* words;
+    string words;
     struct Node
     {
         uint32_t key;
@@ -17,16 +17,47 @@ private:
     Node* nodes;
 
 public:
-    explicit SerializableHashMap(int size = 101);
-    /*
-    bool contains( const Node & x) const;
-    void makeEmpty();
-    bool insert( const Node & x);
-    bool insert( Node && x);
-    bool remove( const Node & x);
-    */
-    
+    explicit SerializableHashMap(const string & fileName):tables{nullptr},Node{nullptr}
+    {  
+        words = "";
+        ifstream inFile;
+        inFile.open(fileName);
+        
+        if (inFile.is_open())
+        {   
+            cout << "Successfully opened dict.txt" << endl;
+        }else
+        {
+            cout << "Unable to open file dict.txt" << endl;
+            exit(-1);
+        }
+        string line;
+        while (!inFile.eof())
+        {
+            getline(inFile,line);
+            if (inFile.good())
+            {
+                words += line + ",";
+                //hashMap.add(line);
+            }
 
+        }
+        words = words[0,words.length-2];
+        inFile.close();     
+        /*
+        bool contains( const Node & x) const;
+        void makeEmpty();
+        bool insert( Node && x);
+        bool remove( const Node & x);
+        */
+    }
+
+    bool insert( const string & x)
+    {
+        
+
+    }
+    
 
 
 
@@ -43,29 +74,6 @@ int main()
     // If not empty,
     // use chaining and change the node.next. 
    
-    ifstream inFile;
-    inFile.open("dict.txt");
-    
-    if (inFile.is_open())
-    {   
-        cout << "Successfully opened dict.txt" << endl;
-    }else
-    {
-	    cout << "Unable to open file dict.txt" << endl;
-	    exit(-1);
-    }
-    string line;
-    while (!inFile.eof())
-    {
-        getline(inFile,line);
-        if (inFile.good())
-        {
-            cout << line << endl;
-            //hashMap.add(line);
-        }
-
-    }
-    inFile.close();     
     //    SerializableHashMap m("dict.txt");
 
     return 0;
