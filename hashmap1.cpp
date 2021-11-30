45631dd0ea134f7bb689ef3195d8a843d249d429
togashi13 <caizh@umich.edu>
2021-11-30 00:28:58 -0500

    new main

--- hashmap1.cpp
+++ hashmap1.cpp
@@ -76,5 +76,22 @@ int main()
    
     //    SerializableHashMap m("dict.txt");
 
+    string word;
+    HashMapLinearChaining dict1(50000);
+    while (!words.feof())
+    {
+        words >> word;
+        dict1.add(word);
+    }
+    dict1.hist();
+    ifstream spell("words.dat");
+    string s;
+    while (!spell.eof())
+    {
+        spell >> s;
+        if (dict1.contains(s)) cout << s << " found !\n";
+        else cout << s << " not found !\n";
+    }
+    
     return 0;
 }
